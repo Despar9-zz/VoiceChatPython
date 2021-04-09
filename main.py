25c6a4f9adade14e0176c76806489a141a49c6cb
yanL0752 <80777895+yanL0752@users.noreply.github.com>
2021-04-10 00:58:43 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from vidstream import AudioSender
+from vidstream import AudioReceiver
+import threading
+import socket
+import numpy
+
+ip = socket.gethostbyname(socket.gethostname())
+
+receiver = AudioReceiver(ip, 9999)
+receive_thread = threading.Thread(target=receiver.start_server)
+
+sender = AudioSender("", 5555)
+sender_thread = threading.Thread(target=sender.start_stream)
+
+receive_thread.start()
+sender_thread.start()
+
+
